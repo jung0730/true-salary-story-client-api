6cebfe1cd23190c420e303945dde74f9a237caee
Pitt Wu <kgb00128@gmail.com>
2023-04-21 23:01:43 +0800

    feat: user profile api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('config/passport');
+const User = require('models/User');
+const jwtAuthMiddleware = require('middlewares/jwtAuthMiddleware');
 
 router.get(
   '/google',
@@ -14,5 +16,33 @@ router.get(
     res.redirect(`${process.env.FRONTEND_URL}/login?token=${req.user.token}`);
   },
 );
+
+router.get('/profile', jwtAuthMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select(
+      'displayName email profilePicture',
+    );
+    res.status(200).json({
+      status: 'success',
+      message: 'User data retrieved successfully',
+      data: { user },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Server error',
+      data: null,
+    });
+  }
+});
+
+router.post('/logout', (req, res) => {
+  res.clearCookie('token');
+  res.status(200).json({
+    status: 'success',
+    message: 'User logged out successfully',
+    data: null,
+  });
+});
 
 module.exports = router;
