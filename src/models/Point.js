eec4d0cbf9ea6cee746d6d7bc41fb2baf15383ce
Pitt Wu <kgb00128@gmail.com>
2023-04-30 14:25:21 +0800

    feat: 擴充新增積分功能的 DB 欄位，並新增一隻每日簽到的 api 用來測試積分功能是否正常

---
+++ src/models/Point.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const PointSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  point: {
+    type: Number,
+    default: 0,
+  },
+  lastCheckIn: {
+    type: Date,
+  },
+});
+
+const Point = mongoose.model('Point', PointSchema);
+
+module.exports = Point;
