d139c1f9eaf54e7f5969298b537ccc8fc94fadb5
W.H. Lin <is.wh.lin@gmail.com>
2023-05-10 23:12:32 +0800

    feat(salary): Update salary schema and add salary endpoints

--- src/routes/salary.js
+++ src/routes/salary.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
 const Salary = require('models/Salary');
+const mongoose = require('mongoose');
+const isValidObjectId = mongoose.Types.ObjectId.isValid;
+const jwtAuthMiddleware = require('middleware/jwtAuthMiddleware');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.post('/salary', async (req, res) => {
+router.post('/salary', jwtAuthMiddleware, async (req, res) => {
   const salary = new Salary(req.body);
 
   try {
@@ -32,9 +35,15 @@ router.post('/salary', async (req, res) => {
   }
 });
 
-router.get('/salary/:id', async (req, res) => {
+router.get('/salary/:id', jwtAuthMiddleware, async (req, res) => {
+  const id = req.params.id;
+
+  if (!isValidObjectId(id)) {
+    return res.status(400).json({ message: 'id 格式錯誤' });
+  }
+
   try {
-    const salary = await Salary.findById(req.params.id).select('-_id -__v');
+    const salary = await Salary.findById(id);
     if (!salary) {
       return res.status(404).json({
         message: 'Salary not found',
@@ -47,7 +56,6 @@ router.get('/salary/:id', async (req, res) => {
       result: salary,
     });
   } catch (error) {
-    console.error(error);
     return res.status(500).json({
       message: 'Server error',
       result: [],
