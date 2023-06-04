5e271c67cd6c693b9c3e17fe9eb37982b0004264
Mischa <mischa0619@gmail.com>
2023-06-04 16:38:07 +0800

    feat: test send message to other user

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -1,23 +1,62 @@
 const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({ port: 8080 });
+const connections = new Map();
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
   ws.on('message', (data) => {
-    data = data.toString();
-    console.log(data);
+    const message = JSON.parse(data);
 
-    // ws.send(data);
+    if (message.type === 'userId') {
+      const { userId } = message;
 
-    let clients = wss.clients;
-    clients.forEach((client) => {
-      client.send(data);
-    });
+      if (connections.has(userId)) {
+        console.log(
+          `User ${userId} is already connected. Rejecting new connection.`,
+        );
+        return;
+      }
+
+      ws.userId = userId;
+      connections.set(userId, ws);
+      console.log(`User ${userId} connected.`);
+    } else if (message.type === 'chat') {
+      const { content, receiverId } = message;
+      const senderId = ws.userId;
+      console.log(
+        `Received message from ${senderId} to ${receiverId}: ${content}`,
+      );
+
+      const receiverSocket = connections.get(receiverId);
+      const senderSocket = connections.get(senderId);
+
+      if (receiverSocket) {
+        const message = {
+          type: 'chat',
+          content,
+          senderId,
+        };
+
+        receiverSocket.send(JSON.stringify(message));
+        senderSocket.send(JSON.stringify(message));
+      } else {
+        console.log(`Receiver ${receiverId} is not connected.`);
+      }
+    }
+
+    console.log('ws: -----', ws.userId);
   });
 
   ws.on('close', () => {
     console.log('Close connected');
+
+    const userId = ws.userId;
+
+    if (userId) {
+      connections.delete(userId);
+      console.log(`User ${userId} disconnected.`);
+    }
   });
 });
