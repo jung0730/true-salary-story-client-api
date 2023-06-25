239eb4a7c58996288c129d6459da9b0e172dbfde
pitt.wu <pitt.wu@sincheng.net>
2023-06-25 15:16:36 +0800

    test: app.js allow samesite

--- src/app.js
+++ src/app.js
@@ -42,6 +42,8 @@ app.use(
     saveUninitialized: true,
     cookie: {
       secure: process.env.NODE_ENV === 'production' ? true : false,
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     },
   }),
 );
