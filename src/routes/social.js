a72dd1461f1ef17abefe4bbf7f8f002b870129e5
jung0730 <arj2763@gmail.com>
2024-02-06 12:05:24 +0800

    slightly adjust social.js

--- src/routes/social.js
+++ src/routes/social.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 const passport = require('passport');
 const FRONTEND_URL = process.env.FRONTEND_URL;
@@ -10,30 +9,24 @@ router.get(
 );
 
 router.get('/google/callback', (req, res, next) => {
-  passport.authenticate('google', { session: false }, (err, user, info) => {
+  passport.authenticate('google', { session: false }, (err, user) => {
     if (err) {
       return next(err);
     }
-
     if (!user) {
       return next({ message: 'Authentication failed', statusCode: 401 });
     }
 
-    // Create a JSON object containing the tokens
     const tokens = {
       token: user.token,
       refreshToken: user.refreshToken,
     };
-
-    // Base64 encode the JSON object
     const encodedTokens = Buffer.from(JSON.stringify(tokens)).toString(
       'base64',
     );
-
-    res.redirect(`${FRONTEND_URL}/login?tokens=${encodedTokens}`);
+    const redirectURL = `${FRONTEND_URL}/login?tokens=${encodedTokens}`;
+    res.redirect(redirectURL);
   })(req, res, next);
 });
-
-// 預期擴增 Line Login
 
 module.exports = router;
